622115abd84bc21d2116e083bce9ac916b4ce41e
Rod Vagg <rod@vagg.org>
2019-07-20 15:34:41 +1000

    retry fetch with exponential backoff

--- tests/registry.js
+++ tests/registry.js
@@ -1,6 +1,20 @@
-var assert = require('assert'),
-    registry = require('../lib/registry');
-
+var assert = require('assert');
+var request = require('request');
+request._get = request.get
+var triggerRequestError = null
+var triggerRequestNoJSON = null
+request.get = function (options, callback) {
+  if (triggerRequestError) {
+    callback(new Error('derp'))
+    return triggerRequestError()
+  }
+  if (triggerRequestNoJSON) {
+    callback(null, 'yoiks', null)
+    return triggerRequestNoJSON()
+  }
+  request._get.apply(this, arguments)
+}
+var registry = require('../lib/registry');
 
 describe('registry', function () {
     describe('split()', function () {
@@ -30,6 +44,7 @@ describe('registry', function () {
     describe('get()', function () {
         it('should retrieve valid data from the default registry', function (done) {
             this.timeout(5000);
+
             registry.get({id: 'async'}, function (err, json, change) {
                 assert.ok(!err);
                 assert.equal(json.name, 'async');
@@ -37,6 +52,30 @@ describe('registry', function () {
                 done();
             });
         });
-    });
+
+        it('failure retry', function (done) {
+            this.timeout(5000);
+
+            var triggeredError = false
+            var triggeredNoJSON = false
+            triggerRequestError = () => {
+              triggeredError = true
+              triggerRequestError = null
+              triggerRequestNoJSON = () => {
+                triggeredNoJSON = true
+                triggerRequestNoJSON = null
+              }
+            }
+
+            registry.get({id: 'async'}, function (err, json, change) {
+              assert.ok(!err, 'should not error!');
+              assert.equal(json.name, 'async');
+              assert.deepEqual(change, {id: 'async'});
+              assert.ok(triggeredError, 'sanity check error triggered')
+              assert.ok(triggeredNoJSON, 'sanity check bad json triggered')
+              done();
+           });
+        })
+    });      
 });
 
